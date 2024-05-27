7e1e7e73f1aab3f86f832ceb2d18df901947dced
Manikanta-Reddy-14237 <manikanta14237@gmail.com>
2024-05-27 19:26:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,9 +21,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.json());
-app.use(cors({
-  origin: 'https://elearningfrontend-9hdgsjuvw-manikantas-projects-f78ee616.vercel.app'
-}));
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
