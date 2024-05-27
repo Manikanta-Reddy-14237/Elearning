b7a1b124380fd4384d3d4ff01382b933abbb4b8b
Manikanta-Reddy-14237 <manikanta14237@gmail.com>
2024-05-27 20:48:47 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ import authorizeRole from './authorizeRole.js';
 
 
 const app = express();
-app.use(cors({origin:["https://elearningbackend-jg1p54tf9-manikantas-projects-f78ee616.vercel.app"}));
+app.use(cors({ origin: [""] }));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
