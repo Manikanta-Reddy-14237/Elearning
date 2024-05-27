4e74e621e1621a7e7258746b866a2db829237321
Manikanta-Reddy-14237 <manikanta14237@gmail.com>
2024-05-27 14:11:31 +0530

    Update teacherdata.js

--- frontend/src/DBdata/teacherdata.js
+++ frontend/src/DBdata/teacherdata.js
@@ -20,7 +20,7 @@ function TeacherDetails() {
                     return navigate("/login");
                 }
                 var token = localStorage.getItem('authToken');
-                const resourceData =await axios.get(`https://elearning-khaki.vercel.app/Teacherdata`, {
+                const resourceData =await axios.get(`http://elearning-khaki.vercel.app/Teacherdata`, {
                     headers: {
                         Authorization: `Bearer ${token}`, 
                     },
@@ -40,7 +40,7 @@ function TeacherDetails() {
 
     const removeTeacher = async (teacherId) => {
         try {
-            await axios.post('https://elearning-khaki.vercel.app/teacherdelete', { teacherId }, {
+            await axios.post('http://elearning-khaki.vercel.app/teacherdelete', { teacherId }, {
                 headers: {
                     Authorization: `Bearer ${token}`,
                 },
