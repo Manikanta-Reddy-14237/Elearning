bfd19c26ecfecc335986778a6beb7cc7116204fc
Manikanta-Reddy-14237 <manikanta14237@gmail.com>
2024-05-28 10:04:33 +0530

    Update Studentdashboard.js

--- frontend/src/Dashboards/Studentdashboard.js
+++ frontend/src/Dashboards/Studentdashboard.js
@@ -45,7 +45,7 @@ function StudentDashboard() {
                 return;
             }
             try {
-                const response = await axios.get("https://elearningbackend-manikantas-projects-f78ee616.vercel.app/studentdata", {
+                const response = await axios.get("https://elearningbackend-git-main-manikantas-projects-f78ee616.vercel.app/studentdata", {
                     headers: {
                         Authorization: `Bearer ${token}`,
                     },
@@ -70,7 +70,7 @@ function StudentDashboard() {
                 return;
             }
             try {
-                const response = await axios.get('https://elearningbackend-manikantas-projects-f78ee616.vercel.app/teacher/files', {
+                const response = await axios.get('https://elearningbackend-git-main-manikantas-projects-f78ee616.vercel.app/teacher/files', {
                     headers: {
                         Authorization: `Bearer ${token}`,
                     },
@@ -104,7 +104,7 @@ function StudentDashboard() {
 
     const fetchSignedUrl = async (filename) => {
         try {
-            const response = await axios.get(`https://elearningbackend-manikantas-projects-f78ee616.vercel.app/generate-signed-url/${filename}`, {
+            const response = await axios.get(`https://elearningbackend-git-main-manikantas-projects-f78ee616.vercel.app/generate-signed-url/${filename}`, {
                 headers: {
                     Authorization: `Bearer ${token}`,
                 },
@@ -125,7 +125,7 @@ function StudentDashboard() {
             if (isVideoFile(filename)) {
                 fetchSignedUrl(filename);
             } else {
-                setLink(`https://elearningbackend-manikantas-projects-f78ee616.vercel.app/public/uploads/${filename}`);
+                setLink(`https://elearningbackend-git-main-manikantas-projects-f78ee616.vercel.app/public/uploads/${filename}`);
             }
         }
     };
